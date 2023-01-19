e1d153dd8db8f04e5a417c741b1ffa92741f35e1
Yuri Edward <yuri6037@outlook.com>
2023-01-19 15:28:27 +0100

    Added docs for fs feature

--- core/src/fs/mod.rs
+++ core/src/fs/mod.rs
@@ -26,6 +26,9 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+//! This module provides cross-platform functions to hide, unhide files, manage file extensions and
+//! get the most compatible absolute path of a file.
+
 #[cfg(unix)]
 mod unix;
 
